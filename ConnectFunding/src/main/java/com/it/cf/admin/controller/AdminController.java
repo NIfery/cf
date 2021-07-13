67d23bb92df0f10124d46803f21347c1251eb97d
back531 <back531@naver.com>
2021-07-13 16:21:55 +0900

    admin commit

---
+++ ConnectFunding/src/main/java/com/it/cf/admin/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.it.cf.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+
+	@RequestMapping("/admin/index")
+	public void index() {
+		
+	}
+}

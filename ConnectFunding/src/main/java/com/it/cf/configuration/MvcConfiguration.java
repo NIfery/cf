ae0e94b7d5776a6c2b4571d6184a7a02705a3b50
HwangMinJi-star <85562388+HwangMinJi-star@users.noreply.github.com>
2021-07-17 13:19:23 +0900

    MvcConfiguration.java 복붙

--- ConnectFunding/src/main/java/com/it/cf/configuration/MvcConfiguration.java
+++ ConnectFunding/src/main/java/com/it/cf/configuration/MvcConfiguration.java
@@ -6,35 +6,33 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 import com.it.cf.admin.controller.AdminLoginInterceptor;
 import com.it.cf.user.controller.LoginInterceptor;
-
 
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer{
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		
-	registry.addInterceptor(new LoginInterceptor())
-	.excludePathPatterns("/cfmember/cfLogin","/cfmember/register")
-	.addPathPatterns("/cfmember/*");
-	}
+   @Override
+   public void addInterceptors(InterceptorRegistry registry) {
 
-	registry.addInterceptor(new AdminLoginInterceptor())
-	.excludePathPatterns("/admin/login/adminLogin")
-	.addPathPatterns("/admin/**");
-	}
+      registry.addInterceptor(new LoginInterceptor())
+      .excludePathPatterns("/cfmember/cfLogin","/cfmember/register","/cfmember/jusoPopup")
+      .addPathPatterns("/cfmember/*");
 
-	@Bean
-	public CommonsMultipartResolver multipartResolver() {
-		CommonsMultipartResolver multipartResolver 
-			= new CommonsMultipartResolver();
-		multipartResolver.setDefaultEncoding("UTF-8"); // 파일 인코딩 설정
-		multipartResolver.setMaxUploadSizePerFile(2 * 1024 * 1024); // 파일당 업로드 크기 제한 (2MB)
-		
-		return multipartResolver;
-	}
-	
+      registry.addInterceptor(new AdminLoginInterceptor())
+      .excludePathPatterns("/admin/login")
+      .addPathPatterns("/admin/**");
+
+   }
+
+   @Bean
+   public CommonsMultipartResolver multipartResolver() {
+      CommonsMultipartResolver multipartResolver 
+      = new CommonsMultipartResolver();
+      multipartResolver.setDefaultEncoding("UTF-8"); // 파일 인코딩 설정
+      multipartResolver.setMaxUploadSizePerFile(2 * 1024 * 1024); // 파일당 업로드 크기 제한 (2MB)
+
+      return multipartResolver;
+   }
+
 }
